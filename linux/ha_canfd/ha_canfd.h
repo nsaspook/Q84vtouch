0d82c2f41e51e54a9c1e7de15b14691b94c305fa
Your Name <you@example.com>
2025-08-09 10:37:34 -0700

    change ipaddress

--- linux/ha_canfd/ha_canfd.h
+++ linux/ha_canfd/ha_canfd.h
@@ -53,12 +53,12 @@ extern "C" {
 #define HR_SEC  3600
 #define DAY_SEC  HR_SEC*24
 
-#define LOG_VERSION     "v1.15"
+#define LOG_VERSION     "v1.16"
 #define MQTT_VERSION    "V3.11"
 #ifdef __amd64
 #define ADDRESS         "tcp://10.1.1.172:1883"
 #else
-#define ADDRESS         "tcp://10.1.1.30:1883"
+#define ADDRESS         "tcp://10.1.1.172:1883"
 #endif
 #define CLIENTID        "MateQ84_Mqtt"
 #define TOPIC_P         "mateq84/data/solar"
